c1ed52474f713c43346be20b6b485c0d02a13af5
Michael Chen <m@perspectivezoom.com>
2012-07-20 11:18:35 -0700

    hello world

---
+++ app/models/url.rb
@@ -0,0 +1,3 @@
+class Url < ActiveRecord::Base
+  attr_accessible :long_url, :short_url
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Urlshorten::Application.config.secret_token = 'de5cd23d3c0086c11e4894c69beef44924b68c7aa17ca4952bea5a75d72c548608450c559c80ec228109e4e53f2aa6cc50d0ae388e88217d61ffe6850f56248c'
